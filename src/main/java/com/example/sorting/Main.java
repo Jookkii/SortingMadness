26e190ca51c91623a404f729866649cbea3dd772
mishchunio <143113233+mishchunio@users.noreply.github.com>
2024-12-07 13:49:12 +0100

    Initial Maven

---
+++ src/main/java/com/example/sorting/Main.java
@@ -0,0 +1,13 @@
+package com.example.sorting;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("Sorting Application Started!");
+        System.out.println("Welcome to Sorting Application!");
+    }
+}
---
+++ src/test/java/com/example/sorting/MainTest.java
@@ -0,0 +1,13 @@
+package com.example.sorting;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class MainTest {
+
+    @Test
+    public void testMain() {
+        assertTrue("Simple test passed!", true);
+    }
+}
