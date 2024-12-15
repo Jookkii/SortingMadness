39936b7129cbc795b26b7c5494dec5a4e82b052d
Jookkii <143968529+Jookkii@users.noreply.github.com>
2024-12-15 15:52:52 +0100

    logger api controller

--- src/main/java/com/example/sorting/controller/ApiController.java
+++ src/main/java/com/example/sorting/controller/ApiController.java
@@ -1,9 +1,12 @@
 package com.example.sorting.controller;
 
+import com.example.sorting.Main;
 import com.example.sorting.dto.SortResult;
 import com.example.sorting.service.*;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,10 +16,13 @@ import java.util.ArrayList;
 @RestController
 public class ApiController {
     private final SortContext sortContext;
+    private static final Logger logger = LoggerFactory.getLogger(ApiController.class);
 
     @Autowired
     public ApiController(SortContext sortContext) {
         this.sortContext = sortContext;
+        logger.debug("Przypisano kontekst sortowań");
+        logger.info("Zakończono inicjalizację kontrolera");
     }
 
 
@@ -26,21 +32,25 @@ public class ApiController {
             @RequestBody String requestBody
     ) {
         String[] algorithmList = algorithms.split(",");
+        logger.debug("Odebrano i odczytano listę algorytmów");
         List<SortResult> results = new ArrayList<>();
         JsonObject jsonObject = JsonParser.parseString(requestBody).getAsJsonObject();
+        logger.debug("Przetworzono request body na obiekt typu Json");
+        logger.info("Odebrano przekazane parametry");
 
         for (String algorithm : algorithmList) {
             String sortedArrayJson = sortContext.sort(algorithm, jsonObject);
-
+            logger.debug("wykorzystano strategie:"+algorithm);
             JsonObject sortedResult = JsonParser.parseString(sortedArrayJson).getAsJsonObject();
             List<String> sortedArray = new ArrayList<>();
             sortedResult.getAsJsonArray("sortedArray").forEach(element -> sortedArray.add(element.getAsString()));
+            logger.debug("utworzono json z wynikiem działania strategii:"+algorithm);
 
             long executionTime = sortedResult.get("executionTime").getAsLong();
 
             results.add(new SortResult(algorithm, executionTime, sortedArray));
         }
-
+        logger.info("zakończono wykonywanie algorytmów");
         return results;
     }
 
